c46f8e0a6fc429baea8d8b2251ef5f2310b47260
Silverneo <cmzhang7@hotmail.com>
2014-06-16 19:29:08 +0800

    solution for the next prime problem

---
+++ prime.py
@@ -0,0 +1,39 @@
+#!usr/bin/python
+"""
+Find the next prime number until the user chooses to stok asking for the next
+one
+"""
+from math import sqrt
+
+def isPrime(number):
+    if number > 1:
+        if number == 2:
+            return True
+        if number % 2 == 0:
+            return False
+        for divisor in range(3, int(sqrt(number)+1), 2):
+            if number % divisor == 0:
+                return False
+        return True
+    return False
+
+def gen_primes():
+    yield 2
+    number = 3
+    while(True):
+        if isPrime(number):
+            yield number
+        number += 2
+
+def main():
+    generator = gen_primes()
+    while True:
+        ans =raw_input("Press Y to generate the next prime, N to exit:")
+
+        if ans == 'Y':
+            print generator.next()
+        else:
+            break
+
+if __name__ == '__main__':
+    main()
